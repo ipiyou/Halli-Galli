b57119b440a026cd2b8c4b2d7e6ed4ea1933a272
ipiyou <tao710803@gmail.com>
2023-03-23 12:16:34 +0900

    feat: init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router/dist/lib/components";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="ingame" element={<div />} />
+    </Routes>
+  );
+}
+
+export default App;
