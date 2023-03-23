005bbae1c3ea015078b9ec3f16e56878e44ee6f7
ipiyou <tao710803@gmail.com>
2023-03-23 12:20:51 +0900

    feat: styled component 추가

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HomePage = () => {
+  return <_Wrapper></_Wrapper>;
+};
+
+const _Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
